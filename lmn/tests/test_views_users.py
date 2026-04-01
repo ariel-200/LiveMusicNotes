0117e3221687abe569b0e0084c8b081a1eae9e0e
somiseol <somi.seol@proton.me>
2026-04-01 14:56:10 -0500

    wrong test; now tests that user cannot edit a user profile when logged out

--- lmn/tests/test_views_users.py
+++ lmn/tests/test_views_users.py
@@ -120,8 +120,8 @@ class Logout(TestCase):
         response = self.client.post(logout, follow=True)
         self.assertTemplateUsed('home.html')
 
-    def test_logged_out_user_cannot_add_note(self):
+    def test_logged_out_user_cannot_user_profile(self):
         """ verify that a user must be logged in to create new notes """
-        response = self.client.get(reverse('new_note'))
+        response = self.client.get(reverse('user_profile'))  # NOT A NEW TEMPLATE
 
         self.assertNotContains(response, 'Edit')  # double-check 

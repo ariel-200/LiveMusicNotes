ce8aff779660ae10fcef6c1fa525eabb58503c48
somiseol <somi.seol@proton.me>
2026-04-01 13:12:13 -0500

    added logoff confirmation

---
+++ lmn/static/js/confirm-logout.js
@@ -0,0 +1,10 @@
+var logoutButtons = document.querySelectorAll('#logout-button')
+
+logoutButtons.forEach(function(button){
+    button.addEventListener('click', function(ev){
+        var okToLogout = confirm("Confirm Log Out?");
+        if (!okToLogout) {
+            ev.preventDefault();
+        }
+    })
+})

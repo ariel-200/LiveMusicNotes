7a88310522a128179bdf041e7161989fd2ab8633
abiir <rd0387oi@go.minnstate.edu>
2026-04-01 15:11:29 -0500

    pass current time to notes for show template

--- lmn/views/views_notes.py
+++ lmn/views/views_notes.py
@@ -43,7 +43,7 @@ def notes_for_show(request, show_pk):
     """ Get notes for one show, most recent first. """
     show = get_object_or_404(Show, pk=show_pk)  
     notes = Note.objects.filter(show=show_pk).order_by('-posted_date')
-    return render(request, 'lmn/notes/notes_for_show.html', {'show': show, 'notes': notes, 'now': timezone.now()})
+    return render(request, 'lmn/notes/notes_for_show.html', {'show': show, 'notes': notes, 'now': timezone.now() })
 
 
 def note_detail(request, note_pk):

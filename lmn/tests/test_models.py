4cb89a210a816f9f80705e9bfe85b910638be951
somiseol <somi.seol@proton.me>
2026-04-06 11:20:03 -0500

    started creating tests for show model

--- lmn/tests/test_models.py
+++ lmn/tests/test_models.py
@@ -20,3 +20,21 @@ class TestUser(TestCase):
         user2 = User(username='bob', email='bob@bob.com', first_name='bob', last_name='bob')
         with self.assertRaises(IntegrityError):
             user2.save()
+
+class TestShow(TestCase):
+
+    def test_create_show_same_venue_same_time_fails(self):
+        # make sure that creating 2 different shows at the same time at the same venue location fails
+        pass
+
+    def test_create_show_same_venue_overlap_time_fails(self):
+        # make sure that creating 2 different shows at the overlapping times at the same venue location fails
+
+        pass
+    def test_create_show_different_venue_same_time_same_artist_fails(self):
+        # make sure that creating 2 different shows with the same artist performing at the same time at different venues is not possible
+        pass
+
+    def test_create_second_show_starting_at_the_end_of_first(self):
+        # should be able to create shows that start at the exact end time of a previous show at the same venue
+        pass

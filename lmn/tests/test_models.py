241fa6bff81617f9b8fcd8dd4b483553615ba34a
abiir <rd0387oi@go.minnstate.edu>
2026-04-07 11:02:05 -0500

    add model test

--- lmn/tests/test_models.py
+++ lmn/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import timedelta
+from lmn.models import Artist,Venue,Show,Note
 
 
 class TestUser(TestCase):
@@ -20,3 +24,17 @@ class TestUser(TestCase):
         user2 = User(username='bob', email='bob@bob.com', first_name='bob', last_name='bob')
         with self.assertRaises(IntegrityError):
             user2.save()
+
+class TestNoteModel(TestCase):
+
+    def setUp(self):
+        self.artist = Artist.objects.create(name='Test Artist')
+        self.venue = Venue.objects.create(name='Test Venue', city='Minneapolis', state='MN')
+        self.user = User.objects.create(username='testuser', email='test@example.com', first_name='Test', last_name='User')
+
+    def test_cannot_create_note_for_future_show(self):
+        show = Show.objects.create(artist=self.artist, venue=self.venue, show_date=timezone.now() + timedelta(days=5))
+        note = Note(show=show,user=self.user, title='test title', text='test text')
+        with self.assertRaises(ValidationError):
+            note.full_clean()
+

420bad771eff56d7f7d7cc6d0863b4d5e6323dbb
somiseol <somi.seol@proton.me>
2026-05-05 09:55:48 -0500

    turned star font size into const

--- lmn/static/js/star-ratings.js
+++ lmn/static/js/star-ratings.js
@@ -1,5 +1,6 @@
 function renderStars() {
     const maxStars = 5;
+    const starSize = "18px"
     const ratingElements = document.querySelectorAll('.note-rating');
 
     ratingElements.forEach(el => {  // loop through each el found
@@ -10,10 +11,9 @@ function renderStars() {
             const emptyStars = '☆'.repeat(Math.max(0, maxStars - Math.floor(ratingValue)));
             
             el.textContent = filledStars + emptyStars;
-            el.style.fontSize = "18px";
+            el.style.fontSize = starSize;
             
             el.style.color = "#3452ae";  // match colorscheme
-            //el.style.fontWeight = "bold";
         }
     });
 }
